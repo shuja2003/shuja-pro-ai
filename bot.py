a1e3bfc1fcfb0833f1a7ff729c7904d030a2a471
shuja2003 <shuja.nining@gmail.com>
2026-08-05 00:33:01 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,21 @@
-# Shuja Pro AI v1
+# Shuja Pro AI v2
 # Main Bot Controller
 
 import time
 
-from price import get_btc_price
+from price import (
+    get_btc_price,
+    get_candles
+)
+
 from strategy import analyze_market
+
 from papertrade import (
     open_paper_trade,
     close_paper_trade,
     get_statistics
 )
+
 from telegram_bot import send_message
 
 
@@ -17,7 +23,7 @@ def run_bot():
 
     print("🚀 Shuja Pro AI Started")
 
-    while True:
+    for _ in range(3):
 
         price = get_btc_price()
 
@@ -25,51 +31,90 @@ def run_bot():
             time.sleep(5)
             continue
 
-        analysis = analyze_market(None)
+
+        candles = get_candles()
+
+        if candles is None:
+            print("No candle data")
+            time.sleep(10)
+            continue
+
+
+        analysis = analyze_market(
+            candles
+        )
+
 
         signal = analysis["signal"]
         confidence = analysis["confidence"]
+
 
         print("\nBTC Price:", price)
         print("Signal:", signal)
         print("Confidence:", confidence)
 
+
         send_message(
-            f"""🤖 Shuja Pro AI
+            f"""
+🤖 Shuja Pro AI
 
 BTCUSDT
+
+Price: {price}
 
 Signal: {signal}
 
 Confidence: {confidence}%
 
-Mode: PAPER TRADING
+Mode:
+PAPER TRADING
 """
         )
 
-        if signal == "🟢 BUY":
+
+        if signal == "🟢 BUY" or signal == "🔴 SELL":
+
+            direction = (
+                "BUY"
+                if signal == "🟢 BUY"
+                else "SELL"
+            )
+
 
             open_paper_trade(
-                "BUY",
+                direction,
                 price,
                 confidence
             )
 
+
             time.sleep(60)
+
 
             exit_price = get_btc_price()
 
-            close_paper_trade(
-                exit_price
-            )
 
-            print(get_statistics())
+            if exit_price:
+
+                close_paper_trade(
+                    exit_price
+                )
+
+
+                print(
+                    get_statistics()
+                )
+
 
         else:
+
             print("No trade")
+
 
         time.sleep(60)
 
 
+
 if __name__ == "__main__":
+
     run_bot()
