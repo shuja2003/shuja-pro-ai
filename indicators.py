3f15eb75fd36121d9a7863837d995a4b6357a406
shuja2003 <shuja.nining@gmail.com>
2026-08-04 23:27:44 +0530

    Update indicators.py

--- indicators.py
+++ indicators.py
@@ -1,40 +1,61 @@
+# Shuja Pro AI v1
+# Technical Indicators Engine
+
 import pandas as pd
 
 
 def calculate_ema(data, period):
-    """
-    Calculate Exponential Moving Average
-    """
-    return data.ewm(
+    return data["close"].ewm(
         span=period,
         adjust=False
     ).mean()
 
 
 def calculate_rsi(data, period=14):
-    """
-    Calculate RSI
-    """
 
-    delta = data.diff()
+    delta = data["close"].diff()
 
-    gain = delta.where(
-        delta > 0, 0
-    )
+    gain = delta.clip(lower=0)
+    loss = -delta.clip(upper=0)
 
-    loss = -delta.where(
-        delta < 0, 0
-    )
-
-    avg_gain = gain.rolling(
-        period
-    ).mean()
-
-    avg_loss = loss.rolling(
-        period
-    ).mean()
+    avg_gain = gain.rolling(period).mean()
+    avg_loss = loss.rolling(period).mean()
 
     rs = avg_gain / avg_loss
 
     rsi = 100 - (
-        100 / (1 +
+        100 / (1 + rs)
+    )
+
+    return rsi
+    def calculate_macd(data):
+
+    ema_fast = data["close"].ewm(
+        span=12,
+        adjust=False
+    ).mean()
+
+    ema_slow = data["close"].ewm(
+        span=26,
+        adjust=False
+    ).mean()
+
+    macd = ema_fast - ema_slow
+
+    signal = macd.ewm(
+        span=9,
+        adjust=False
+    ).mean()
+
+    return macd, signal
+
+
+def calculate_volume_strength(data):
+
+    avg_volume = data["volume"].mean()
+
+    current_volume = data["volume"].iloc[-1]
+
+    return (
+        current_volume / avg_volume
+    )
