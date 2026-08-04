1a418442eb69e7428f8c94d8bee9b99afe802b8f
shuja2003 <shuja.nining@gmail.com>
2026-08-04 15:10:09 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,70 @@
+# Shuja Pro AI v1
+# Main Bot Controller
+
+import time
+
+from price import get_btc_price
+from strategy import analyze_market
+from papertrade import (
+    open_paper_trade,
+    close_paper_trade,
+    get_statistics
+)
+
+
+def run_bot():
+
+    print("🚀 Shuja Pro AI Started")
+
+    while True:
+
+        price = get_btc_price()
+
+        if price is None:
+            time.sleep(5)
+            continue
+
+
+        analysis = analyze_market(None)
+
+        signal = analysis["signal"]
+        confidence = analysis["confidence"]
+
+
+        print("\nBTC Price:", price)
+        print("Signal:", signal)
+        print("Confidence:", confidence)
+
+
+        if signal == "🟢 BUY":
+
+            open_paper_trade(
+                "BUY",
+                price,
+                confidence
+            )
+
+            time.sleep(60)
+
+            exit_price = get_btc_price()
+
+            close_paper_trade(
+                exit_price
+            )
+
+
+            print(
+                get_statistics()
+            )
+
+
+        else:
+            print("No trade")
+
+
+        time.sleep(60)
+
+
+
+if __name__ == "__main__":
+    run_bot()
